5e7f638a392205db9ee98ffade27e84fbb92dee1
ThotAlion <thomas.peyruse@gmail.com>
2020-07-25 17:58:15 +0100

    resolution de l'install de scipy

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #environnement python 3
 sudo apt-get install git python3-dev python3-pip ipython3 python3-serial
 pip3 install --upgrade pip
-# environnement numpy
-sudo apt-get install libatlas-base-dev
-sudo pip3 install numpy
+
+# environnement scipy (souvent sujet d'erreurs)
+sudo apt-get install python3-scipy
 
 # environnement pypot
 sudo pip3 install pypot
